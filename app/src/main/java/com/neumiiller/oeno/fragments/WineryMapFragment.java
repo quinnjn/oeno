76ee275b150c19409c027143037c92dd9d188067
quinnjn <quinn@neumiiller.com>
2014-10-01 17:30:01 -0600

    converting winerymapfragment to a SupportMapFragment

--- app/src/main/java/com/neumiiller/oeno/fragments/WineryMapFragment.java
+++ app/src/main/java/com/neumiiller/oeno/fragments/WineryMapFragment.java
@@ -1,9 +1,9 @@
 package com.neumiiller.oeno.fragments;
 
-import android.app.Fragment;
+import com.google.android.gms.maps.SupportMapFragment;
 
 /**
  * @author QJN on 2014-09-27.
  */
-public class WineryMapFragment extends Fragment {
+public class WineryMapFragment extends SupportMapFragment {
 }
