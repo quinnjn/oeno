f782d14507c6ec254b068d32fec865377951028a
Quinn Neumiiller <qneumiiller@gasbuddy.com>
2014-10-01 18:24:06 +0000

    Adding a reference the default wineries json stored in assets

--- app/src/main/java/com/neumiiller/oeno/OenoApplication.java
+++ app/src/main/java/com/neumiiller/oeno/OenoApplication.java
@@ -3,7 +3,5 @@ package com.neumiiller.oeno;
 import android.app.Application;
 
 public class OenoApplication extends Application {
-
-
-
+	public static final String DEFAULT_WINERIES_JSON = "wineries.json";
 }
