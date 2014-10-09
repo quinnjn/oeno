0d815b68e26b6d1665b67abe3dbf121fb2937b78
Quinn J Neumiiller <quinn@neumiiller.com>
2014-10-09 09:25:46 -0600

    Adding Events and Promos

---
+++ app/src/main/java/com/neumiiller/oeno/models/Event.java
@@ -0,0 +1,7 @@
+package com.neumiiller.oeno.models;
+
+/**
+ * @author QJN on 2014-10-09.
+ */
+public class Event implements Parcelable {
+}
---
+++ app/src/main/java/com/neumiiller/oeno/models/Promo.java
@@ -0,0 +1,9 @@
+package com.neumiiller.oeno.models;
+
+import com.neumiiller.oeno.Event;
+
+/**
+ * @author QJN on 2014-10-09.
+ */
+public class Promo extends Event implements Parcelable {
+}
