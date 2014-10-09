cc3960d9d0bc45bf555d9d6a5ed7b4115dd38452
Quinn J Neumiiller <quinn@neumiiller.com>
2014-10-09 10:19:17 -0600

    Adding parcelable methods to Event

--- app/src/main/java/com/neumiiller/oeno/models/Event.java
+++ app/src/main/java/com/neumiiller/oeno/models/Event.java
@@ -6,5 +6,33 @@ import android.os.Parcelable;
 /**
  * @author QJN on 2014-10-09.
  */
-public class Event implements Parcelable {
+public class Event implements Parcelable, Parcelable {
+	private String winery
+
+    protected Event(Parcel in) {
+        winery = in.readString();
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(winery);
+    }
+
+    @SuppressWarnings("unused")
+    public static final Parcelable.Creator<Event> CREATOR = new Parcelable.Creator<Event>() {
+        @Override
+        public Event createFromParcel(Parcel in) {
+            return new Event(in);
+        }
+
+        @Override
+        public Event[] newArray(int size) {
+            return new Event[size];
+        }
+    };
 }
