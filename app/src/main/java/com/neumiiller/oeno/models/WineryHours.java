d588efb8c6fc9343c74a4af728d4cc5c50e22b1a
Quinn J Neumiiller <quinn@neumiiller.com>
2014-10-09 10:24:45 -0600

    Adding new files WineryHours and WineryTime

---
+++ app/src/main/java/com/neumiiller/oeno/models/WineryHours.java
@@ -0,0 +1,12 @@
+package com.neumiiller.oeno.models;
+
+import java.util.HashMap;
+import com.neumiiller.oeno.models.WineryTime;
+
+/**
+ * @author QJN on 2014-10-09.
+ */
+public class WineryHours extends HashMap<String, WineryTime> {
+	private int open;
+	private int close;
+}
---
+++ app/src/main/java/com/neumiiller/oeno/models/WineryTime.java
@@ -0,0 +1,9 @@
+package com.neumiiller.oeno.models;
+
+/**
+ * @author QJN on 2014-10-09.
+ */
+public class WineryTime {
+	private int open;
+	private int close;
+}
