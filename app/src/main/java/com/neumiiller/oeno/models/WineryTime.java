f2a9532f264fca8385694be83cd4b3ce7e15f12f
Quinn J Neumiiller <quinn@neumiiller.com>
2014-10-10 10:37:40 -0600

    Implementing WineryTime via TDD

--- app/src/main/java/com/neumiiller/oeno/models/WineryTime.java
+++ app/src/main/java/com/neumiiller/oeno/models/WineryTime.java
@@ -4,6 +4,55 @@ package com.neumiiller.oeno.models;
  * @author QJN on 2014-10-09.
  */
 public class WineryTime {
+	private static final char TIME_SEPERATOR = '-';
+	
+	
+	private class TimeHelper{
+		private int hour;
+		private int min;
+		private String frame;
+		
+		public TimeHelper(int time){
+			this.hour = get12Hour(time);
+			this.min = getMinute(time);
+			this.frame = getFrame(time);
+		}
+		
+		private int getHour(int time){
+			int hour = time/100;
+			return hour;
+		}
+		
+		private int get12Hour(int time){
+			int hour = getHour(time);
+			if(isPm(time) && !isNoon(hour)){
+				hour -= 12;
+			}
+			return hour;
+		}
+		
+		private boolean isNoon(int hour){
+			return hour == 12;
+		}
+		
+		private int getMinute(int time){
+			int min = time % 100;
+			return min;
+		}
+		
+		private String getFrame(int time){
+			return isPm(time) ? "pm" : "am"; 
+		}
+
+		private boolean isPm(int time){
+			return getHour(time) >= 12;
+		}
+		
+		public String getTimeStamp(){
+			return String.format("%d:%02d %s", hour, min, frame);
+		}
+	}
+	
 	private int open;
 	private int close;
 
@@ -15,12 +64,27 @@ public class WineryTime {
 	public String getHourString(){
 		if(isClosed()){
 			return "Closed";
-		}
+		}	
 
-		return null;
+		return generateHourString();
 	}
 	
 	private boolean isClosed(){
 		return close < open;
+	}
+	
+	private String generateHourString(){
+		TimeHelper openTime = new TimeHelper(open);
+		TimeHelper closeTime = new TimeHelper(close);
+		
+		String openTimeStamp = openTime.getTimeStamp();
+		String closeTimeStamp = closeTime.getTimeStamp();
+		
+		return String.format(
+				"%s %s %s", 
+				openTimeStamp,
+				TIME_SEPERATOR,
+				closeTimeStamp
+		);
 	}
 }
