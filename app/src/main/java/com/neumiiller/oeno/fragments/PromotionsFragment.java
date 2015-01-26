e77adfccc77ae1d43cd3e23526a6c694323ebbe7
Quinn Neumiiller <quinnjn@gmail.com>
2015-01-26 10:22:33 -0600

    Adding a no promotion view for promotions fragment

--- app/src/main/java/com/neumiiller/oeno/fragments/PromotionsFragment.java
+++ app/src/main/java/com/neumiiller/oeno/fragments/PromotionsFragment.java
@@ -1,10 +1,21 @@
 package com.neumiiller.oeno.fragments;
 
 import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.neumiiller.oeno.R;
 
 /**
  * @author QJN on 2014-10-28.
  */
 public class PromotionsFragment extends Fragment {
 
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_promotions, null);
+        return view;
+    }
 }
