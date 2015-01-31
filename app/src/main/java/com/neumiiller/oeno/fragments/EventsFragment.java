c899547e88841fa969c6ddc653a70bd6f6be3bc9
Quinn Neumiiller <quinnjn@gmail.com>
2015-01-31 15:31:12 -0600

    Adding the layout to the events fragment.

--- app/src/main/java/com/neumiiller/oeno/fragments/EventsFragment.java
+++ app/src/main/java/com/neumiiller/oeno/fragments/EventsFragment.java
@@ -1,10 +1,22 @@
 package com.neumiiller.oeno.fragments;
 
 import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.neumiiller.oeno.R;
 
 /**
  * @author QJN on 2014-10-28.
  */
 public class EventsFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_events, null);
+        return view;
+    }
 
 }
